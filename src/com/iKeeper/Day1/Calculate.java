0f38784f1493cbd9d44f58ea1f85179f20c8dfbf
dianin <xyg1940@gmail.com>
2023-03-30 19:15:59 +0900

    docs:readme 초본

---
+++ src/com/iKeeper/Day1/Calculate.java
@@ -0,0 +1,16 @@
+package com.iKeeper.Day1;
+import java.util.*;
+import java.io.*;
+public class Calculate {
+int n= Integer.parseInt(st.nextToken());
+int m= Integer.parseInt(st.nextToken());
+int sign = br.next().CharAt();
+    //생성자 메소드
+    void Add(){};
+    void Sub(){};
+    void Mul(){};
+    void Div(){};
+    void result(){};
+    public Calculate(){};
+    public Calculate(String){};
+}
--- src/com/iKeeper/Main.java
+++ src/com/iKeeper/Main.java
@@ -1,8 +1,13 @@
 package com.iKeeper;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
+public class run{
+}
     public static void main(String[] args) {
-	// write your code here
+	BufferedReader br = new BufferedReader();
+
+    // write your code here
     }
 }
