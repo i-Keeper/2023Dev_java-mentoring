5a0ae71966cfa50ee75bef9e01e572034d0a20d6
dianin <xyg1940@gmail.com>
2023-03-31 15:44:07 +0900

    refactor:전반적인 구조변경 및 완성

--- src/com/iKeeper/Day1/Calculate.java
+++ src/com/iKeeper/Day1/Calculate.java
@@ -1,22 +1,51 @@
 package com.iKeeper.Day1;
 
-public class Calculate {
-    private int n;
-    private int m;
+class Calculator {
+    public static int calculate(String expression) {
+        char[] tokens = expression.toCharArray();
+        int n = 0;
+        int m = 0;
+        char operator = '\0';
+        int result = 0;
+        for (char c : tokens) {
+            if (Character.isDigit(c)) {
+                if (operator == '\0') {
+                    n = n * 10 + Character.getNumericValue(c);
+                } else {
+                    m = m * 10 + Character.getNumericValue(c);
+                }
+            } else {
+                if (operator == '\0') {
+                    operator = c;
+                } else {
+                    n = calculate(n, m, operator);
+                    m = 0;
+                    operator = c;
+                }
+            }
+        }
+        result = calculate(n, m, operator);
+        return result;
+    }
 
-    Calculate(){
-        this.n=n;
-        this.m=m;
-
+    private static int calculate(int n, int m, char operator) {
+        switch (operator) {
+            case '+':
+                return n + m;
+            case '-':
+                return n - m;
+            case '*':
+                return n * m;
+            case '/':
+                if (m == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return 0;
+                } else {
+                    return n / m;
+                }
+            default:
+                System.out.println("올바른 연산이 아닙니다.");
+                return 0;
+        }
     }
-    public static int Add(int n, int m){
-        return n+m;
-    }
-    public static int Sub(int n, int m) {
-        return n-m;
-    }
-    public static int Mul(int n, int m){
-        return n*m;
-    }
-    public static int Div(int n, int m) {return n/m; }
 }
--- src/com/iKeeper/Day1/Problem1.java
+++ src/com/iKeeper/Day1/Problem1.java
@@ -3,39 +3,10 @@ import java.util.*;
 import java.io.*;
 
 public class Problem1 {
-    public static void main(String []args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = bf.readLine();
-        String[] tokens = str.split("[+\\-*/]");
-
-        int n = Integer.parseInt(tokens[0]);
-        char c = str.replaceAll("[^+\\-*/]", "").charAt(0);
-        int m = Integer.parseInt(tokens[1]);
-        int result =0;
-
-
-        Calculate myCalculate = new Calculate();
-                switch(c){
-                    case'+':
-                        result =Calculate.Add(n,m);
-                        break;
-                    case'-':
-                        result = Calculate.Sub(n,m);
-                        break;
-                    case'*':
-                        result = Calculate.Mul(n,m);
-                        break;
-                    case'/':
-                        if(m==0){
-                            System.out.println("0으로 나눌 수 없습니다.");
-                        }
-                        else{
-                        result = Calculate.Div(n,m);
-                        break;}
-                    default:
-                        System.out.println("올바른 연산이 아닙니다.");
-            }
-        System.out.println(result);
+        public static void main (String[]args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String expression = br.readLine().replaceAll(" ", "");
+            int result = Calculator.calculate(expression);
+            System.out.println(result);
+        }
     }
-}
