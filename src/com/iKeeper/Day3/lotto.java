19d8de2bdd79ecf6e9e8143c49fbdf4d5512ab3e
dianin <xyg1940@gmail.com>
2023-04-06 20:06:44 +0900

    feat:입력된 돈 1000원당 로또 1장으로 변환기능 구현

--- src/com/iKeeper/Day3/lotto.java
+++ src/com/iKeeper/Day3/lotto.java
@@ -8,10 +8,14 @@ public class lotto {
 
 }
 class Input_money{
-    public Input_money() throws IOException {
+    private int insert_money;
+    public void Input_money() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int insert_money = Integer.parseInt(br.readLine());
+        insert_money = Integer.parseInt(br.readLine());
         System.out.println(insert_money+"원이 입력 되었습니다.");
-
+    }
+    public void Exchange_lotto(){
+        int exchange_lotto = insert_money/1000;
+        System.out.println(exchange_lotto+"장의 로또를 구매합니다.");
     }
 }
