0ba9a975c5aca5053d7361a61f30ab1f0c3b7ac2
dianin <xyg1940@gmail.com>
2023-04-06 19:58:52 +0900

    feat:사용자 돈 입력기능구현

--- src/com/iKeeper/Day3/lotto.java
+++ src/com/iKeeper/Day3/lotto.java
@@ -1,8 +1,16 @@
 package com.iKeeper.Day3;
-
+import java.util.*;
+import java.io.*;
 public class lotto {
     public static void main(String []args){
 
     }
 
+}
+class Input_money{
+    public Input_money() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int insert_money = Integer.parseInt(br.readLine());
+
+    }
 }
