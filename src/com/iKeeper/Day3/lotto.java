5617af3a23924a7844003b01c2387bf65c7b526d
dianin <xyg1940@gmail.com>
2023-04-09 19:29:09 +0900

    feat:자동로또 구매기능구현

--- src/com/iKeeper/Day3/lotto.java
+++ src/com/iKeeper/Day3/lotto.java
@@ -21,7 +21,28 @@ class Lottogame {
         System.out.println(insert_money + "원이 입력 되었습니다.");
         exchange_lotto=insert_money/1000;
         System.out.println(exchange_lotto + "장의 로또를 구매합니다.");
+        Selectlottonumber(exchange_lotto);
     }
+    public void Selectlottonumber(int exchange_lotto) throws IOException {
+        this.exchange_lotto=exchange_lotto;
+        int[] user_lotto = new int[6];
+        while(exchange_lotto!=0) {
+            for (int i = 0; i < 6; i++) {
+                int num = (int) (Math.random() * 45) + 1;    // 1~46까지의 임의의 수를 받는다.
+                user_lotto[i] = num;
+                for (int j = 0; j < i; j++) {    // 중복된 번호가 있으면 이전 포문으로 돌아가 다시 시행한다.
+                    if (user_lotto[i] == user_lotto[j]) {
+                        i--;
+                        break;
+                    }
 
+                }
+                System.out.print(user_lotto[i] + " ");    // 로또번호를 출력한다.
+
+            }
+            System.out.println();
+            exchange_lotto--;
+        }
+    }
 
 }
