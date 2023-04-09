5ecb9fe78b5b99f39c0debec499b33cda7f8e810
dianin <xyg1940@gmail.com>
2023-04-09 19:21:58 +0900

    refactor:돈입력 기능 수정

--- src/com/iKeeper/Day3/lotto.java
+++ src/com/iKeeper/Day3/lotto.java
@@ -1,21 +1,27 @@
 package com.iKeeper.Day3;
-import java.util.*;
 import java.io.*;
+
 public class lotto {
-    public static void main(String []args){
+    public static void main(String []args) throws IOException {
+        new Lottogame();
 
     }
+
 
 }
-class Input_money{
-    private int insert_money;
-    public void Input_money() throws IOException {
+class Lottogame {
+    int insert_money;
+    int exchange_lotto;
+    private BufferedReader br;
+
+
+    public Lottogame() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        insert_money = Integer.parseInt(br.readLine());
-        System.out.println(insert_money+"원이 입력 되었습니다.");
+        int insert_money = Integer.parseInt(br.readLine());
+        System.out.println(insert_money + "원이 입력 되었습니다.");
+        exchange_lotto=insert_money/1000;
+        System.out.println(exchange_lotto + "장의 로또를 구매합니다.");
     }
-    public void Exchange_lotto(){
-        int exchange_lotto = insert_money/1000;
-        System.out.println(exchange_lotto+"장의 로또를 구매합니다.");
-    }
+
+
 }
